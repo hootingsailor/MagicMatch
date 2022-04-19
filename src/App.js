da49be03f75c5e2af1734415274c4b37d44e5814
hootingsailor <sharmaaakash824@gmail.com>
2022-04-19 22:23:07 +0530

    update cards and add some functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard'
 
 const cardImages = [
 	{ "src": "/img/helmet-1.png" },
@@ -14,22 +15,48 @@ function App() {
 
 	const [cards , setCards] = useState([])
 	const [turns , setTurns] = useState(0)
+	const [choiceOne , setChoiceOne] = useState(null)	
+	const [choiceTwo , setChoiceTwo] = useState(null)
 
+	// shuffle cards
 	const shuffleCards = ()=> {
-		const images = [...cardImages , ...cardImages]
+		const shuffledCards = [...cardImages , ...cardImages]
 		.sort(()=> Math.random()-0.5)
 		.map((card)=>({...card , id : Math.random() }))
 
-		setCards(images)
+		setCards(shuffledCards)
 		setTurns(0)
 	}
-	console.log(cards , turns)
+
+	// handle a choice
+	const handleChoice = (card)=>{
+		choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+	}
+
+	// compare 2 selected cards
+	useEffect(() => {
+		if(choiceOne && choiceTwo){
+			if(choiceOne.src === choiceTwo.src){
+				
+			}
+		}
+	}, [choiceOne, choiceTwo])
+
+	// reset choice and increase turn
+	const resetTurns = ()=> {
+		setChoiceOne(null)
+		setChoiceTwo(null)
+		setTurns(prevturns => prevturns + 1)
+	}
+
 	return (
 		<div className="App">
 			<h1>Magic Match</h1>
 			<button onClick={shuffleCards}>New Game</button>
 			<div className='card-grid'>
-				  
+				  {cards.map(card => (
+					<SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+				  ))}
 			</div>
 		</div>
 	);
---
+++ src/components/SingleCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './SingleCard.css'
+
+export default function SingleCard({card , handleChoice}) {
+
+    const handleClick = ()=>{
+        handleChoice(card)
+    }
+
+    return (
+        <div className='card'>
+            <div>
+                <img className="front" src={card.src} alt="card front" />
+                <img className="back" src="/img/cover.png" onClick={handleClcik} alt="card back" />
+            </div>
+        </div>
+    )
+}
