93797feee4511f301058025700bcd44be2bbad13
hootingsailor <sharmaaakash824@gmail.com>
2022-04-20 10:18:43 +0530

    add features to card

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import SingleCard from './components/SingleCard'
 
 const cardImages = [
-	{ "src": "/img/helmet-1.png" },
-	{ "src": "/img/potion-1.png" },
-	{ "src": "/img/ring-1.png" },
-	{ "src": "/img/scroll-1.png" },
-	{ "src": "/img/shield-1.png" },
-	{ "src": "/img/sword-1.png" }
+	{ "src": "/img/helmet-1.png" , matched : false },
+	{ "src": "/img/potion-1.png" , matched : false },
+	{ "src": "/img/ring-1.png" , matched : false },
+	{ "src": "/img/scroll-1.png" , matched : false },
+	{ "src": "/img/shield-1.png" , matched : false },
+	{ "src": "/img/sword-1.png" , matched : false }
 ]
 
 function App() {
@@ -37,10 +37,20 @@ function App() {
 	useEffect(() => {
 		if(choiceOne && choiceTwo){
 			if(choiceOne.src === choiceTwo.src){
+				setCards(prevCards => {
+					return prevCards.map(card => {
+						if(card.src === choiceOne.src){
+							return {...card , matched : true}
+						}
+						else{
+							return card
+						}
+					})
+				})
 				resetTurn()
 			}
 			else{
-				resetTurn()
+				setTimeout(() => resetTurn(), 1000) 
 			}
 		}
 	}, [choiceOne, choiceTwo])
@@ -58,7 +68,11 @@ function App() {
 			<button onClick={shuffleCards}>New Game</button>
 			<div className='card-grid'>
 				  {cards.map(card => (
-					<SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+					<SingleCard key={card.id} 
+					card={card} 
+					handleChoice={handleChoice} 
+					flipped={card === choiceOne || card === choiceTwo || card.matched} 
+					/>
 				  ))}
 			</div>
 		</div>
